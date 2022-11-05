712234cc1bc89d254ab038182461c8ae3ec6b3cf
Tim Krassowski <tkrass1@gmail.com>
2022-11-05 03:10:09 +0000

    project updates and experimentation

--- EC2NameGenerator2.py
+++ EC2NameGenerator2.py
@@ -2,32 +2,32 @@ import random
 import string
 import sys
 
-def string_generator (size =10, string=string.digits + string.ascii_letters):
-    return "".join(random.choice(string) for _ in range(size))
-    
-department = input("Enter Department: Accounting, FinOps, Marketing: ")
+def string_generator(size=10, string=string.ascii_letters + string.digits):
+    return ''.join(random.choice(string) for _ in range(size))
+
+department = input("Enter department: Accounting, FinOps, Marketing: ")
 
 for _ in department:
-    if department == "Accounting" or department.lower() == 'accounting' :
+    if department == "Accounting" or department.lower() == "accounting" :
         print("Processing... ")
         break
-    elif department == "FinOps" or department.lower() == 'finops' :
+    elif department == "FinOps" or department.lower() == "finops" :
         print("Processing... ")
         break
     elif department == "Marketing" or department.lower() == "marketing" :
         print("Processing... ")
         break
     else:
-        print("Error: This Name Generator is only for specific departments. Please enter appropriate department. ")
+        print("Error: This name generator is only for specific departments. Please enter appropriate department. ")
         raise SystemExit
-
+    
 number = int(input("Enter desired number of EC2 Instances: "))
 
 if number < 1:
-    print("Please enter a number greater than 0. ")
+    print("Please enter a number greater than 0")
 elif number > 0:
     print("")
-    
+
 print("EC2 Instance Names Generated: ")
 
 for _ in range(1, number + 1):
