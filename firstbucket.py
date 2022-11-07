72361f1641dc7c0703c783231b2e1bdf8e3928ab
Tim Krassowski <tkrass1@gmail.com>
2022-11-07 03:55:03 +0000

    uploading to s3

--- firstbucket.py
+++ firstbucket.py
@@ -1,12 +1,8 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("week14testbucket1")
+bucket=aws_resource.Bucket("week14testbucket3")
 response = bucket.create(
-    ACL='private',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2'
-    },
-   
+    ACL='public-read'
 )
 
 print(response)
--- uploadingfilestoS3bucket.py
+++ uploadingfilestoS3bucket.py
@@ -3,5 +3,6 @@ import boto3
 s3_resource=boto3.client("s3")
 s3_resource.upload_file(
     Filename="Finn.JPG",
-    Bucket="Week14testbucket",
-    Key="Finntest.jpg")
+    Bucket="week14testbucket3",
+    Key="Finntest.JPG"
+)
