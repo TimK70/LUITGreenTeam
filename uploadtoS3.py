52cde19e2ca1d4955608a76f9ae4c3f1c56afadd
Tim Krassowski <tkrass1@gmail.com>
2022-11-08 03:11:17 +0000

    Upload multiple files to S3

--- uploadtoS3.py
+++ uploadtoS3.py
@@ -1,21 +1,24 @@
 import boto3
 import os
-s3_resource=boto3.client("s3")
-s3_resource.upload_file(
-    Filename="/home/ec2-user/environment/LUITGreenTeam/Finn.JPG",
-    Bucket="week14testbucket3",
-    Key="Finn.JPG"
-)
+#s3_resource=boto3.client("s3")
+#s3_resource.upload_file(
+#    Filename="/home/ec2-user/environment/LUITGreenTeam/Finn.JPG",
+#    Bucket="week14testbucket3",
+#    Key="Finn.JPG"
+#)
+
 #how to upload multiple files at once.
-import os
+
 import glob
 cwd=os.getcwd()
-cwd=cwd+"/upload/"
-
-files=glob.glob(cwd+"/home/ec2-user/environment/LUITGreenTeam/*.JPG")
+cwd=cwd+"/LUITGreenTeam/"
+files=glob.glob(cwd+"*.JPG")
+files
 for file in files:
-    s3_resource=boto3.client("s3")
+    s3_resource = boto3.client("s3")
     s3_resource.upload_file(
     Filename=file,
     Bucket="week14testbucket3",
-    Key=file.split("/"))
+    Key=file.split("/")[-1])
+
+
